c5c0251e71e37105e50cc4081406b9700e334b94
zildeos <zildeuos@me.com>
2023-04-24 18:59:42 +0300

    basic gol done

---
+++ src/grid.c
@@ -0,0 +1,95 @@
+#include <grid.h>
+#include <array.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Grid* Grid(int i)
+{
+  struct Grid *grid = malloc(sizeof (struct Grid));
+  grid->cellsPerSide = i;
+  grid->cellsMap = Array(sizeof(float)*2);
+  float p[2];
+  for (int y = 0; y<=i; y++) {
+    for (int x = 0; x<=i; x++) {
+      p[0] = (x/(float)i);
+      p[1] = 1.0f - (y/(float)i);
+      ArrayPush(grid->cellsMap,p);
+    }
+  }
+  grid->activeCells= malloc(sizeof(unsigned int)*grid->cellsPerSide*grid->cellsPerSide);
+  grid->_tempactiveCells= malloc(sizeof(unsigned int)*grid->cellsPerSide*grid->cellsPerSide);
+  for (int y=0;y<grid->cellsPerSide;y++)
+  {
+    for (int x=0;x<grid->cellsPerSide;x++)
+    {
+      grid->activeCells[(y*grid->cellsPerSide)+x] = 0;
+    }
+  }
+  grid->borderMap = Array(sizeof(float)*2);
+  for (int y=0;y<=grid->cellsPerSide;y++)
+  {
+    ArrayPush(grid->borderMap,ArrayGetElement(grid->cellsMap,(y*(i+1))));
+    ArrayPush(grid->borderMap,ArrayGetElement(grid->cellsMap,(y*(i+1))+i));
+  }
+  for (int x=0;x<=grid->cellsPerSide;x++)
+  {
+    ArrayPush(grid->borderMap,ArrayGetElement(grid->cellsMap,x));
+    ArrayPush(grid->borderMap,ArrayGetElement(grid->cellsMap,(i*(i+1))+x));
+  }
+  grid->changed = 0;
+  grid->cellsElements = malloc(sizeof(unsigned int)*grid->cellsPerSide*grid->cellsPerSide*6);
+  grid->cellsElementsCount = 0;
+  grid->cellUpdateProc = 0;
+  return grid;
+}
+void GridUpdate(struct Grid* grid)
+{
+  int gridElementsCounter = 0;
+  grid->changed = 0;
+  grid->cellsElementsCount = 0;
+  for (int i =0; i<grid->cellsPerSide*grid->cellsPerSide;i++) {
+    if(grid->activeCells[i])
+    {
+      int xIndex = ((i/grid->cellsPerSide) * (grid->cellsPerSide+1)) + i%grid->cellsPerSide;
+      int yIndex = xIndex+(grid->cellsPerSide+1);
+      unsigned int cell[4] = {xIndex,xIndex+1,yIndex,yIndex+1};
+      grid->cellsElements[gridElementsCounter*6 + 0] = cell[0];
+      grid->cellsElements[gridElementsCounter*6 + 1] = cell[3];
+      grid->cellsElements[gridElementsCounter*6 + 2] = cell[1];
+      grid->cellsElements[gridElementsCounter*6 + 3] = cell[0];
+      grid->cellsElements[gridElementsCounter*6 + 4] = cell[2];
+      grid->cellsElements[gridElementsCounter*6 + 5] = cell[3];
+      gridElementsCounter++;
+    }
+  }
+  grid->cellsElementsCount = gridElementsCounter*6;
+}
+void GridCallProc(struct Grid* grid)
+{
+  char newGrid[grid->cellsPerSide*grid->cellsPerSide];
+  for (int y=0;y<grid->cellsPerSide;y++)
+  {
+    for (int x=0;x<grid->cellsPerSide;x++)
+    {
+      newGrid[(y*grid->cellsPerSide)+x] = grid->cellUpdateProc(grid,x,y);
+    }
+  }
+  memcpy(grid->activeCells,newGrid,grid->cellsPerSide*grid->cellsPerSide*sizeof(char));
+}
+char GridGetCell(struct Grid* grid,unsigned x,unsigned y)
+{
+  if((0>x||x>=grid->cellsPerSide)||(0>y||y>=grid->cellsPerSide))
+    return 0;
+  return grid->activeCells[(y*grid->cellsPerSide)+x];
+}
+void GridSetCellsProc(struct Grid* grid,char (*cellProc)(struct Grid*,int,int))
+{
+  grid->cellUpdateProc = cellProc;
+}
+void GridSetCell(struct Grid* grid,unsigned x,unsigned y,char value)
+{
+  if(0>x||x>grid->cellsPerSide||0>y||y>grid->cellsPerSide)
+    return;
+  grid->activeCells[(y*grid->cellsPerSide)+x] = value==0?0:1;
+  grid->changed = 1;
+}
--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <grid.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <vertexarray.h>
@@ -7,35 +8,37 @@
 #include <texture.h>
 #include <math.h>
 #include <stdio.h>
+#include <grid.h>
 #include <stdlib.h>
+#include <windows.h>
 #include <string.h>
 
 float deltaTime;
-double currTime;
+double currTime,prevTime;
 
-struct Point{
-  float x,y;
-};
-struct Grid{
-  int cellsPerRow;
-  struct Array* cells;
-};
-struct Array* CreateCells(int i)
-{
-  struct Array* cells = Array(sizeof(struct Point));
-  struct Point p;
-  for (int y = 0; y<=i; y++) {
-    for (int x = 0; x<=i; x++) {
-      p.x = (x/(float)i);
-      p.y = 1.0f - (y/(float)i);
-      ArrayPush(cells,&p);
-    }
-  }
-  return cells;
-}
 float clamp(float f,float a,float b)
 {
   return f<a?a:b<f?b:f;
+}
+char testProc(struct Grid* grid,int x,int y)
+{
+  char isAlive = GridGetCell(grid,x,y);
+  int count = 0;
+  count+=GridGetCell(grid, x+1,y);
+  count+=GridGetCell(grid, x-1,y);
+  count+=GridGetCell(grid, x,y+1);
+  count+=GridGetCell(grid, x,y-1);
+  count+=GridGetCell(grid, x+1,y+1);
+  count+=GridGetCell(grid, x-1,y+1);
+  count+=GridGetCell(grid, x+1,y-1);
+  count+=GridGetCell(grid, x-1,y-1);
+  if(!isAlive)
+  {
+    return count==3;
+  }
+  else {
+    return (count==2||count==3)?1:0;
+  }
 }
 int main()
 {
@@ -51,122 +54,34 @@ int main()
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
   GLFWwindow* win;
-  win = glfwCreateWindow(480,480, "game of life",NULL,NULL);
+  win = glfwCreateWindow(600,600, "game of life",NULL,NULL);
   glfwMakeContextCurrent(win);
   gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
   struct Shader *shader = Shader("./../res/default.vs","./../res/default.fs");
   struct VertexArray *gridVa,*borderVa;
   gridVa = VertexArray();
   borderVa = VertexArray();
-  int cellsPerRow = 10;
-  struct Array* cells = CreateCells(cellsPerRow);
-  VertexArrayAddBuffer(gridVa, 0,cells->data,cells->size*cells->count,cells->size, 0);
+  int cellsPerRow = 20;
+  struct Grid* grid = Grid(cellsPerRow);
+  GridSetCellsProc(grid, testProc);
+  VertexArrayAddBuffer(gridVa, 0,grid->cellsMap->data,grid->cellsMap->size*grid->cellsMap->count,grid->cellsMap->size, 0);
   VertexArrayFormatBufferElement(gridVa, 0, TYPE_FLOAT, 2);
-  char activeSquares[cellsPerRow*cellsPerRow];
-  for (int y=0;y<cellsPerRow;y++)
-  {
-    for (int x=0;x<cellsPerRow;x++)
-    {
-      activeSquares[(y*cellsPerRow)+x] = 0;
-      if (x==y) {
-      activeSquares[(y*cellsPerRow)+x] = 1;
-      }
-    }
-  }
-
-  //struct Array *gridElements;
-  //gridElements = Array(sizeof(unsigned int));
-  //for (int i =0; i<cellsPerRow*cellsPerRow;i++) {
-  //  if(activeSquares[i])
-  //  {
-  //    int xIndex = ((i/cellsPerRow) * (cellsPerRow+1)) + i%cellsPerRow;
-  //    int yIndex = xIndex+(cellsPerRow+1);
-  //    unsigned int cell[4] = {xIndex,xIndex+1,yIndex,yIndex+1};
-  //    ArrayPush(gridElements,&cell[0]);
-  //    ArrayPush(gridElements,&cell[3]);
-  //    ArrayPush(gridElements,&cell[1]);
-  //    ArrayPush(gridElements,&cell[0]);
-  //    ArrayPush(gridElements,&cell[2]);
-  //    ArrayPush(gridElements,&cell[3]);
-  //  }
-  //}
-  unsigned int gridElements[cellsPerRow*cellsPerRow*6];
-  //for (int i =0; i<cellsPerRow*cellsPerRow;i++) {
-  //  if(activeSquares[i])
-  //  {
-  //    int xIndex = ((i/cellsPerRow) * (cellsPerRow+1)) + i%cellsPerRow;
-  //    int yIndex = xIndex+(cellsPerRow+1);
-  //    unsigned int cell[4] = {xIndex,xIndex+1,yIndex,yIndex+1};
-  //    gridElements[i*6 + 0] = cell[0];
-  //    gridElements[i*6 + 1] = cell[3];
-  //    gridElements[i*6 + 2] = cell[1];
-  //    gridElements[i*6 + 3] = cell[0];
-  //    gridElements[i*6 + 4] = cell[2];
-  //    gridElements[i*6 + 5] = cell[3];
-  //  }
-  //  else {
-  //    gridElements[i*6 + 0] = 0;
-  //    gridElements[i*6 + 1] = 0;
-  //    gridElements[i*6 + 2] = 0;
-  //    gridElements[i*6 + 3] = 0;
-  //    gridElements[i*6 + 4] = 0;
-  //    gridElements[i*6 + 5] = 0;
-  //  }
-  //}
-
-  //VertexArrayAddBuffer(gridVa, 1,gridElements->data,gridElements->size*gridElements->count,gridElements->size, 0);
-  VertexArrayAddBuffer(gridVa, 1,gridElements,sizeof(gridElements),sizeof(unsigned int), 0);
+  VertexArrayAddBuffer(gridVa, 1,grid->cellsElements,grid->cellsElementsCount * sizeof(unsigned int),sizeof(unsigned int), 0);
   VertexArraySetElementBuffer(gridVa,1);
   float scale = 1.0f;
-  struct Array* borderElements;
-  borderElements = Array(sizeof(struct Point));
-  for (int y=0;y<=cellsPerRow;y++)
-  {
-    ArrayPush(borderElements,ArrayGetElement(cells,(y*(cellsPerRow+1))));
-    ArrayPush(borderElements,ArrayGetElement(cells,(y*(cellsPerRow+1))+cellsPerRow));
-  }
-  for (int x=0;x<=cellsPerRow;x++)
-  {
-    ArrayPush(borderElements,ArrayGetElement(cells,x));
-    ArrayPush(borderElements,ArrayGetElement(cells,(cellsPerRow*(cellsPerRow+1))+x));
-  }
-  VertexArrayAddBuffer(borderVa, 0,borderElements->data,borderElements->size*borderElements->count,borderElements->size, 0);
-  VertexArrayAddBuffer(borderVa, 0,borderElements->data,borderElements->size*borderElements->count,borderElements->size, 0);
+  VertexArrayAddBuffer(borderVa, 0,grid->borderMap->data,grid->borderMap->size*grid->borderMap->count,grid->borderMap->size, 0);
+  VertexArrayAddBuffer(borderVa, 0,grid->borderMap->data,grid->borderMap->size*grid->borderMap->count,grid->borderMap->size, 0);
   VertexArrayFormatBufferElement(borderVa, 0, TYPE_FLOAT, 2);
   float aspect = 5.0f/4.0f;
   float offsetx = 0.0f , offsety = 0.0f;
   int limit = 5;
   char mDown=0;
-  char changed = 1;
+  glfwSetInputMode(win,GLFW_STICKY_KEYS,GLFW_FALSE);
   while (!glfwWindowShouldClose(win)){
-    if(changed)
-    {
-      changed = 0;
-      for (int i =0; i<cellsPerRow*cellsPerRow;i++) {
-        if(activeSquares[i])
-        {
-          int xIndex = ((i/cellsPerRow) * (cellsPerRow+1)) + i%cellsPerRow;
-          int yIndex = xIndex+(cellsPerRow+1);
-          unsigned int cell[4] = {xIndex,xIndex+1,yIndex,yIndex+1};
-          gridElements[i*6 + 0] = cell[0];
-          gridElements[i*6 + 1] = cell[3];
-          gridElements[i*6 + 2] = cell[1];
-          gridElements[i*6 + 3] = cell[0];
-          gridElements[i*6 + 4] = cell[2];
-          gridElements[i*6 + 5] = cell[3];
-        }
-        else {
-          gridElements[i*6 + 0] = 0;
-          gridElements[i*6 + 1] = 0;
-          gridElements[i*6 + 2] = 0;
-          gridElements[i*6 + 3] = 0;
-          gridElements[i*6 + 4] = 0;
-          gridElements[i*6 + 5] = 0;
-        }
-      }
-      VertexArrayUpdateBuffer(gridVa, 1, sizeof(gridElements), gridElements);
-      VertexArraySetElementBuffer(gridVa,1);
-    }
+    prevTime = currTime;
+    GridUpdate(grid);
+    VertexArrayUpdateBuffer(gridVa, 1, grid->cellsElementsCount*sizeof(unsigned int), grid->cellsElements);
+    VertexArraySetElementBuffer(gridVa,1);
     int width,height;
     glfwGetWindowSize(win, &width,&height);
     aspect = (float)height/(float)width;
@@ -180,32 +95,33 @@ int main()
       offsetx+=deltaTime;
     if(glfwGetKey(win,GLFW_KEY_W))
       offsety+=deltaTime;
+    if (glfwGetKey(win,GLFW_KEY_S) == GLFW_RELEASE)
+        mDown = 1;
     if (glfwGetKey(win,GLFW_KEY_S))
+    {
+      GridCallProc(grid);
       offsety-=deltaTime;
+      Sleep(100);
+    }
     if (glfwGetKey(win,GLFW_KEY_ESCAPE))
       glfwSetWindowShouldClose(win,GLFW_TRUE);
 
-    if(glfwGetMouseButton(win,GLFW_MOUSE_BUTTON_1) == GLFW_RELEASE)
-      mDown = 0;
-    if(glfwGetMouseButton(win,GLFW_MOUSE_BUTTON_1) == GLFW_PRESS && !mDown)
+    if((glfwGetMouseButton(win,GLFW_MOUSE_BUTTON_2) == GLFW_PRESS||glfwGetMouseButton(win,GLFW_MOUSE_BUTTON_1) == GLFW_PRESS))
     {
-      mDown = 1;
       double_t mx_d,my_d;
       glfwGetCursorPos(win,&mx_d,&my_d);
       int x = ((mx_d/ width)*cellsPerRow);
       int y = ((my_d/height)*cellsPerRow);
-      printf("Mouse:%d,%d\nSize:%d,%d\nIndex:%d,%d\naspect:%f\n-----------------\n",(int)mx_d,(int)my_d,width,height,x+1,y+1,aspect);
-      changed = 1;
-      activeSquares[(y*cellsPerRow)+x] = 1;
+      GridSetCell(grid,x,y,glfwGetMouseButton(win,GLFW_MOUSE_BUTTON_2) == GLFW_PRESS?0:1);
     }
     glfwSwapBuffers(win);
     glClear(GL_COLOR_BUFFER_BIT);
     VertexArrayBind(gridVa);
     UseShader(shader);
     glUniform1f(1,aspect);
-    glDrawElements(GL_TRIANGLES,cellsPerRow*cellsPerRow*6,GL_UNSIGNED_INT,0);
+    glDrawElements(GL_TRIANGLES,grid->cellsElementsCount*6,GL_UNSIGNED_INT,0);
     VertexArrayBind(borderVa);
-    glDrawArrays(GL_LINES,0,borderElements->count);
+    glDrawArrays(GL_LINES,0,grid->borderMap->count);
     glfwPollEvents();
   }
   glfwDestroyWindow(win);
